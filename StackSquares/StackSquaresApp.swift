1019d66c5d81ca465d0c267f5cacf2f08623fb35
Manan Patel <manan.patel@outlook.com>
2024-09-09 21:54:53 -0600

    Initial Commit

---
+++ StackSquares/StackSquaresApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackSquaresApp.swift
+//  StackSquares
+//
+//  Created by Manan Patel on 2024-09-09.
+//
+
+import SwiftUI
+
+@main
+struct StackSquaresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
